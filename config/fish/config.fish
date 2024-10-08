73d07bf6684c8f82986d64078d0ec136a084c50d
Martin Pritchard <martin@martinpritchard.com>
2024-10-08 23:08:59 +0100

    Show toolbox indicator if within toolbox container

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,18 @@
+function prompt_host
+    set -l containerenv '/run/.containerenv'
+    set -l host prompt_hostname
+    if test -e $containerenv
+        set host (cat $containerenv | grep 'name=' | cut -d\" -f2)
+    end
+    string collect $host
+end
+
+function prompt_is_toolbox
+    if test -e '/run/.toolboxenv'
+        string join '' -- (set_color magenta) ⬢ (set_color normal)
+    end
+end
+
 function fish_prompt
     set -l last_status $status
     set -l stat
@@ -5,13 +20,7 @@ function fish_prompt
         set stat (set_color red) "[$last_status]" (set_color normal)
     end
 
-    set -l containerenv '/run/.containerenv'
-    set -l host prompt_hostname
-    if test -e $containerenv
-        set host (cat $containerenv | grep 'name=' | cut -d\" -f2)
-    end
-
-    string join '' -- (whoami)@$host ' ' (set_color green) (prompt_pwd -D 2) (set_color normal) $stat '>'
+    string join '' -- (prompt_is_toolbox) ' ' (whoami)@(prompt_host) ' ' (set_color green) (prompt_pwd -D 2) (set_color normal) $stat '>'
 end
 
 if status is-interactive
